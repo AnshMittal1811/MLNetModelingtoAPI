b102092a5bc4740f41b9fdf24909bbbbeef96c65
AnshMittal1811 <anshm18111996@gmail.com>
2020-05-05 03:39:48 -0700

    Initial Files for MLNet end to end proj

---
+++ ML_end_2_end/WineAPI/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.ML;
+using System;
+using System.IO;
+using WineCommon;
+
+namespace WineAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(AppContext.BaseDirectory))
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins", buildOptions => { buildOptions.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod(); });
+            });
+
+            services.AddSingleton(Configuration);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddPredictionEnginePool<WineData, WinePrediction>()
+                .FromUri("https://databricksdemostorage.blob.core.windows.net/models/wine.zip");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Use(async (ctx, next) =>
+            {
+                await next();
+                if (ctx.Response.StatusCode == 204)
+                {
+                    ctx.Response.ContentLength = 0;
+                }
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("AllowAllOrigins");
+
+            app.UseMvc();
+        }
+    }
+}
